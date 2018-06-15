7dd799f80621a160309c8c3dae3cf6485d016627
Stefan Lukic <stefan.lukic@yahoo.com>
2018-06-15 18:38:41 +0200

    inicijalizacija

---
+++ src/main/java/poslovnaBanka/PoslovnaBankaApiApp.java
@@ -0,0 +1,18 @@
+package poslovnaBanka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoslovnaBankaApiApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+			SpringApplication.run(PoslovnaBankaApiApp.class, args);
+			
+		}
+
+	}
+
+
