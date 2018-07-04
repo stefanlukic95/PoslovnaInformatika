d4e078c33373be4f85dd515b760af4d3ce1fde7b
Stefan Lukic <stefan.lukic@yahoo.com>
2018-07-04 21:29:03 +0200

    Revert "Dodati racuni"
    This reverts commit 23d26378e670050625bfe8bbf2132340954b5342.

--- src/main/java/poslovnaBanka/racuni/RacuniLicaRepository.java
+++
@@ -1,9 +0,0 @@
-package poslovnaBanka.racuni;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RacuniLicaRepository extends JpaRepository<RacuniLica, Long> {
-}
-
